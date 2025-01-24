69db5dc827256c16ac6553746284b861fb42fb63
prayati01 <prayatipaliwal@gmail.com>
2025-01-24 16:52:56 +0530

    db file

---
+++ vwitsDb.js
@@ -0,0 +1,17 @@
+module.exports = function() {
+    return {
+        products: [
+            { id: 1, name: "Kayak", category: "Watersports", description: "A boat for one person", price: 27.35 },
+            { id: 2, name: "Lifejacket", category: "Watersports", description: "Portective and fashionable", price: 48.95 },
+            { id: 3, name: "Soccer Ball", category: "Soccer", description: "FIFA-approved weight and size", price: 19.50 },
+            { id: 4, name: "Corner Flags", category: "Soccer", description: "Give your field a professional touch", price: 34.95 },
+            { id: 5, name: "Stadium", category: "Soccer", description: "Flat-packed, 35000-seat stadium", price: 87000 },
+            { id: 6, name: "Thinking Cap", category: "Chess", description: "Improve brain efficiency by 87%", price: 16 },
+            { id: 7, name: "Unsteady Chair", category: "Chess", description: "Secretly give your opponent a disadvantage", price: 29.90 },
+            { id: 8, name: "Human Chess Board", category: "Chess", description: "A fun game for the family", price: 75 },
+            { id: 9, name: "Bling Bling King", category: "Chess", description: "Gold-plated, diamond studded King", price: 1300 }
+        ],
+        orders: []
+    }
+}
+ 
