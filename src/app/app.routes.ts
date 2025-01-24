16275802e86dabe69f1b1123b120a17a141f2edc
prayati01 <prayatipaliwal@gmail.com>
2025-01-24 10:47:48 +0530

    adding url paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CartDetailComponent } from './store/cartDetail.component';
+import { CheckoutComponent } from './store/checkout.component';
+import { StoreComponent } from './store/store.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "store", component:StoreComponent},
+    {path:"cart", component: CartDetailComponent},
+    {path: "checkout", component: CheckoutComponent}
+];
+
